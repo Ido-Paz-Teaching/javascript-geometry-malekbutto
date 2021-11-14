21eb76a5d4389d589d356193e0ad29c268f3400e
Malek Butto <malekbutto@gmail.com>
2021-11-14 21:29:45 +0200

    Home Work

--- geometry.js
+++ geometry.js
@@ -1,13 +1,16 @@
 function getTriangleArea(height, width) {
   /* Your amazing code here 🚀 */
+  return ((height*width)/2);
 }
 
 function getSquareArea(side) {
   /* Your amazing code here 🚀 */
+  return Math.pow(side,2);
 }
 
 function getCircleArea(radius) {
   /* Your amazing code here 🚀 */
+  return Number((Math.PI*(math.pow(radius,2))).toFixed(2));
 }
 
 //Please , don't remove the following code 
